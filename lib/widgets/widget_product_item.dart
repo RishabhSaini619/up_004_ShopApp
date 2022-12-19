88e6d4fbad135004649efed8c986555ec36474a1
rishabhsaini619 <rishabhsaini619@gmail.com>
2022-12-19 17:24:55 +0530

    Using 'Consumer' instead of 'Provider.of'

--- lib/widgets/widget_product_item.dart
+++ lib/widgets/widget_product_item.dart
@@ -19,55 +19,57 @@ class ProductItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(30),
-      child: GridTile(
-        // header: Text(productItemId),
-        footer: GridTileBar(
-          backgroundColor: Colors.black87,
-          title: Text(
-            product.productTitle,
-            maxLines: 2,
-            textAlign: TextAlign.center,
-            style: Theme.of(context).textTheme.bodyLarge,
-          ),
-          leading: IconButton(
-            icon: Icon(
-              product.isProductFavorite?
-              Icons.favorite_sharp
-              : Icons.favorite_border_sharp,
-              color: const Color(0xffff0000),
+    // final product = Provider.of<Product>(context);
+    return Consumer<Product>(builder: (context, product, child) {
+      return ClipRRect(
+        borderRadius: BorderRadius.circular(30),
+        child: GridTile(
+          // header: Text(productItemId),
+          footer: GridTileBar(
+            backgroundColor: Colors.black87,
+            title: Text(
+              product.productTitle,
+              maxLines: 2,
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.bodyLarge,
             ),
-            onPressed: () {
-              product.toggleFavoriteStatus();
+            leading: IconButton(
+              icon: Icon(
+                product.isProductFavorite
+                    ? Icons.favorite_sharp
+                    : Icons.favorite_border_sharp,
+                color: const Color(0xffff0000),
+              ),
+              onPressed: () {
+                product.toggleFavoriteStatus();
+              },
+            ),
+            trailing: IconButton(
+              icon: const Icon(
+                Icons.shopping_cart,
+                size: 20,
+                color: Colors.deepOrangeAccent,
+              ),
+              onPressed: () {},
+            ),
+          ),
+          child: GestureDetector(
+            onTap: () {
+              Navigator.of(context).pushNamed(
+                ProductDetailScreen.routeName,
+                arguments: product.productId,
+              );
             },
-          ),
-          trailing: IconButton(
-            icon: const Icon(
-              Icons.shopping_cart,
-              size: 20,
-              color: Colors.deepOrangeAccent,
-            ),
-            onPressed: () {},
-          ),
-        ),
-        child: GestureDetector(
-          onTap: () {
-            Navigator.of(context).pushNamed(
-              ProductDetailScreen.routeName,
-              arguments: product.productId,
-            );
-          },
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(10),
-            child: Image.network(
-              product.productImageURL,
-              fit: BoxFit.contain,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image.network(
+                product.productImageURL,
+                fit: BoxFit.contain,
+              ),
             ),
           ),
         ),
-      ),
-    );
+      );
+    });
   }
 }
